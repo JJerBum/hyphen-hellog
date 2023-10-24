23faabcbdba8b90a2140f0cb54745f5b39b0c209
JJerBum <2hanjunbum6@gmail.com>
2023-10-24 19:01:05 +0900

    feat: create initializer

---
+++ initializer/config.go
@@ -0,0 +1,24 @@
+package initializer
+
+import (
+	"hyphen-hellog/cerrors/exception"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type config interface {
+	Get(string) string
+}
+
+type configImpl struct{}
+
+func (configImpl) Get(key string) string {
+	return os.Getenv(key)
+}
+
+func NewConfig(filenames ...string) config {
+	err := godotenv.Load(filenames...)
+	exception.Sniff(err)
+	return configImpl{}
+}
--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+import "hyphen-hellog/initializer"
+
 func main() {
+	config := initializer.NewConfig()
+	initializer.NewDatabase(config)
 
 }
