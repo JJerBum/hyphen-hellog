09198ddf4cfc1a57ed9571de66fc0a396591966e
JJerBum <2hanjunbum6@gmail.com>
2023-10-28 04:18:47 +0900

    refactor: like controller의 pattern을 과거형에서 현재형으로 변경

--- controller/controller.go
+++ controller/controller.go
@@ -23,8 +23,8 @@ func Route(app *fiber.App) *fiber.App {
 	api.Delete("/posts/comments/comment", middleware.RequireAuth, handlers.DeleteComment)
 
 	// like
-	api.Patch("/posts/:post_id/unliked", middleware.RequireAuth, handlers.UpdateUnliked)
-	api.Patch("/posts/:post_id/liked", middleware.RequireAuth, handlers.UpdateLiked)
+	api.Patch("/posts/:post_id/unlike", middleware.RequireAuth, handlers.UpdateUnliked)
+	api.Patch("/posts/:post_id/like", middleware.RequireAuth, handlers.UpdateLiked)
 
 	return app
 }
